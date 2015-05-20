7f89d544ad867b14c082c70c645018639f6e923c
Ben Longden <blongden@inviqa.com>
2015-05-20 11:30:48 +0100

    Added hobo

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source "https://rubygems.org"
+
+# Chef
+gem "berkshelf", "~> 3.1.4"
+gem "knife-solo", "~> 0.4"
+gem "knife-solo_data_bag", "~> 1.1.0"
+
+# Capistrano
+gem "capistrano", "~> 2.15.5"
+gem "railsless-deploy", "~> 1.1.2"
+
+# Magento
+gem "magentify", "~> 0.0.7"
+
+# Execution helpers
+gem 'rubygems-bundler', '~> 1.4.3'
+
+# CI / testing
+gem 'serverspec', '~> 1.9.0'
---
+++ tools/capistrano/config/deploy.rb
@@ -0,0 +1,17 @@
+set :stages, %w(dev int uat systest production)
+set :default_stage, "dev"
+
+require 'capistrano/ext/multistage'
+require 'inviqa_cap/composer'
+
+set :repository, "git@github.com:inviqa/magento-base-theme.git"
+set :scm, :git
+set :user, "deploy"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :keep_releases, 5
+
+set :linked_files, [ "public/app/etc/local.xml" ]
+set :linked_directories, ["/public/sitemaps", "/public/media", "/public/var", "/public/staging"]
+
+after "deploy:finalize_update", "deploy:cleanup"
