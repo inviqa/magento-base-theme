904c8a2fbee408a5d87afe303d9ece03e0d0fef5
Laura Kishimoto <lkishimoto@sessiondigital.com>
2015-07-07 12:21:19 +0100

    TMUP-607 Fix publicdir path, update readme

--- public/skin/frontend/session/default/gulpfile.js
+++ public/skin/frontend/session/default/gulpfile.js
@@ -1,7 +1,7 @@
 /* set theme name and paths */
 
 var themeName = 'session',
-    publicJsDir = '../../../../../js/',
+    publicJsDir = '../../../../js/',
     toolsDir = '../../../../../tools/',
     patternLabDir = 'pattern-lab/'
 ;
