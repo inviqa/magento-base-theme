eb988665703f474397496b386287460dc75da62f
Katie Fenn <kfenn@inviqa.com>
2015-07-17 12:29:27 +0100

    TMUP-243: Whitespace fixes

--- public/skin/frontend/session/default/js/global.js
+++ public/skin/frontend/session/default/js/global.js
@@ -5,9 +5,9 @@
  * See: http://webpack.github.io/docs/commonjs.html
  */
 var responsiveVideo = require('./session_modules/responsivevideo'),
-	owlCarousel = require('./session_modules/owlCarousel'),
-	css3LimitTextFallback = require('./session_modules/css3limittext'),
-	bindEvent = require('./session_modules/bindevent');
+    owlCarousel = require('./session_modules/owlCarousel'),
+    css3LimitTextFallback = require('./session_modules/css3limittext'),
+    bindEvent = require('./session_modules/bindevent');
 
 /**
  * Initialise global JS dependencies
@@ -22,8 +22,8 @@ css3LimitTextFallback();
  * Session.Global.[function name]([args]);
  */
 if (typeof window.Session == 'undefined') {
-	window.Session = {};
+    window.Session = {};
 }
 window.Session.Global = {
-	bindEvent: bindEvent
+    bindEvent: bindEvent
 };
