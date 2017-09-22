24fb5baf0dafdfea870b82b2b04efb7c45a45c45
fratcliffe <fratcliffe@inviqa.com>
2017-09-22 16:34:46 +0100

    DEVOPS-479: Switch out .local and .dev in remaining tools/ files for .test

--- tools/vagrant/spec/host_spec.rb
+++ tools/vagrant/spec/host_spec.rb
@@ -5,7 +5,7 @@ require 'yaml'
 hosts = Resolv::Hosts.new
 config = YAML::load_file("../hobo/config.yaml")
 describe "Host" do
-  it "mast.dev should route to #{config[:vm_ip]}" do
-    hosts.getaddress("mast.dev").to_s.should eq config[:vm_ip]
+  it "mast.test should route to #{config[:vm_ip]}" do
+    hosts.getaddress("mast.test").to_s.should eq config[:vm_ip]
   end
 end
--- tools/vagrant/spec/httpd_spec.rb
+++ tools/vagrant/spec/httpd_spec.rb
@@ -14,13 +14,13 @@ describe port(80) do
   it { should be_listening }
 end
 
-describe file('/etc/nginx/sites-enabled/mast.dev') do
+describe file('/etc/nginx/sites-enabled/mast.test') do
   it { should be_file }
-  its(:content) { should match /server_name .*mast.dev.*/ }
+  its(:content) { should match /server_name .*mast.test.*/ }
 end
 
 describe "HTTP OK" do
   it do
-    Net::HTTP.get_response(URI.parse('http://mast.dev')).should be_a Net::HTTPOK
+    Net::HTTP.get_response(URI.parse('http://mast.test')).should be_a Net::HTTPOK
   end
 end
